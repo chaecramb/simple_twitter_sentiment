2e8185e23756ebcead9eb91b0ea4c5e5225e9725
Chae Cramb <chaecramb@gmail.com>
2016-11-15 18:02:52 +0000

    refactors to method

--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,11 @@
 from feature_extractor import process_tweets, terms_only
 import vincent
 
-word_freq = process_tweets(terms_only, 'data/stream_trump.json')[0].most_common(20)
-labels, freq = zip(*word_freq)
-data = {'data': freq, 'x': labels}
-bar = vincent.Bar(data, iter_idx='x')
-bar.to_json('term_freq.json')
+def prepare_bar_chart_data(filter_method, filepath):
+    word_freq = process_tweets(filter_method, filepath)[0].most_common(20)
+    labels, freq = zip(*word_freq)
+    data = {'data': freq, 'x': labels}
+    bar = vincent.Bar(data, iter_idx='x')
+    bar.to_json('term_freq.json')
+
+prepare_bar_chart_data(terms_only, 'data/stream_trump.json')
