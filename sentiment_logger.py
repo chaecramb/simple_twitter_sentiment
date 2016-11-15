dd7379c43807149a79e5d5df9a4dc1fbe13e96d2
Chae Cramb <chaecramb@gmail.com>
2016-11-15 19:00:32 +0000

    refactors logger

--- sentiment_logger.py
+++ sentiment_logger.py
@@ -3,51 +3,107 @@ from sentiment_analysis import *
 from datetime import datetime
 import operator
 
-with open("sentiment_log.txt", "a") as f:
+filename = 'sentiment_log.txt'
+
+with open(filename, "a") as f:
     f.write(str(datetime.now()) + '\n\n')
 
     print('Extracting features from tweets...')  
-    common_bigrams = process_tweets(terms_bigrams, 'data/stream_trump.json')[0].most_common(20)
-    common_terms = process_tweets(terms_only, 'data/stream_trump.json')[0].most_common(20)
-
-    f.write('Most common bigrams\n')
-    [f.write('{0}: {1} {2}: {3}\n'.format(i+1, a, b, c)) for i, ((a,b), c) in enumerate(common_bigrams)]
-    f.write('\n')
-
-    f.write('Most common single terms\n')
-    [f.write('{0}: {1}: {2}\n'.format(i+1, t, c)) for i, (t, c) in enumerate(common_terms)]
-    f.write('\n')
+    common_bigrams = process_tweets(terms_bigrams, 
+                     'data/stream_trump.json')[0].most_common(20)
+    common_terms = process_tweets(terms_only, 
+                   'data/stream_trump.json')[0].most_common(20)
 
     print('Calculating frequency of single terms...')
-    frequency_single, number_of_tweets = process_tweets(terms_single, 'data/stream_trump.json')
+    frequency_single, number_of_tweets = process_tweets(terms_single, 
+                                                        'data/stream_trump.json')
+
     print('Calculating frequency of bigrams...')
-    frequency_bigrams, number_of_bigrams = process_tweets(terms_bigrams, 'data/stream_trump.json', bigram=True)
+    frequency_bigrams, number_of_bigrams = process_tweets(terms_bigrams, 
+                                                         'data/stream_trump.json', 
+                                                         bigram=True)
+
     print('Creating frequency of co-occurrence matrix for single terms...')
-    frequency_cooccur_single, _ = process_tweets(terms_cooccurences, 'data/stream_trump.json') 
+    frequency_cooccur_single, _ = process_tweets(terms_cooccurences, 
+                                                 'data/stream_trump.json') 
+
     print('Creating frequency of co-occurrence matrix for bigrams...')
-    frequency_cooccur_bigrams, _ = process_tweets(terms_cooccurences, 'data/stream_trump.json', bigram=True) 
+    frequency_cooccur_bigrams, _ = process_tweets(terms_cooccurences, 
+                                                  'data/stream_trump.json', 
+                                                  bigram=True) 
 
     print('Calculating probabilities for single terms...')
     prob_single, prob_cooccur_single = compute_term_probabilites(frequency_single, 
                                                           frequency_cooccur_single, 
                                                           number_of_tweets)
+
     print('Calculating probabilities for bigrams...')
     prob_bigrams, prob_cooccur_bigrams = compute_term_probabilites(frequency_bigrams, 
                                               frequency_cooccur_bigrams, 
                                               number_of_tweets)
 
     print('Calculating pointwise mutual information for single terms...')
-    single_pmi = compute_pmi(frequency_cooccur_single, prob_cooccur_single, prob_single)
+    single_pmi = compute_pmi(frequency_cooccur_single, 
+                             prob_cooccur_single, 
+                             prob_single)
+  
     print('Calculating pointwise mutual information for bigrams...')
-    bigrams_pmi = compute_pmi(frequency_cooccur_bigrams, prob_cooccur_bigrams, prob_single, prob_bigrams)
+    bigrams_pmi = compute_pmi(frequency_cooccur_bigrams, 
+                              prob_cooccur_bigrams, 
+                              prob_single, 
+                              prob_bigrams)
 
     print('Calculating semantic orientation for single terms...')
-    semantic_orientation_single = compute_semantic_orientation(prob_single, single_pmi, vocab)
+    semantic_orientation_single = compute_semantic_orientation(prob_single, 
+                                                               single_pmi, 
+                                                               vocab)
+ 
     print('Calculating semantic orientation for bigrams...')
-    semantic_orientation_bigrams = compute_semantic_orientation(prob_bigrams, bigrams_pmi, vocab)
+    semantic_orientation_bigrams = compute_semantic_orientation(prob_bigrams, 
+                                                                bigrams_pmi, 
+                                                                vocab)
+  
+    print('Sorting semantic orientation for single terms...')
+    semantic_sorted_single = sorted(semantic_orientation_single.items(), 
+                       key=operator.itemgetter(1), 
+                       reverse=True)
+ 
+    print('Sorting semantic orientation for bigrams...')
+    semantic_sorted_bigrams = sorted(semantic_orientation_bigrams.items(), 
+                       key=operator.itemgetter(1), 
+                       reverse=True)
+
+    print('Extracting most positive single terms...')
+    top_pos_single = [b for b in semantic_sorted_single
+                        if b[0] not in opinion_lexicon.vocab['positive_vocab'] 
+                        and b[0] not in opinion_lexicon.vocab['negative_vocab']
+                     ][:20] 
+    print('Extracting most negative single terms...')
+    top_neg_single = [b for b in semantic_sorted_single
+                        if b[0] not in opinion_lexicon.vocab['positive_vocab'] 
+                        and b[0] not in opinion_lexicon.vocab['negative_vocab']
+                      ][:-21:-1] 
+    print('Extracting most positive bigrams...')
+    top_pos_bigrams = [b for b in semantic_sorted_bigrams 
+                        if b[0][0] not in opinion_lexicon.vocab['positive_vocab'] 
+                        and b[0][1] not in opinion_lexicon.vocab['positive_vocab'] 
+                        and b[0][0] not in opinion_lexicon.vocab['negative_vocab'] 
+                        and b[0][1] not in opinion_lexicon.vocab['negative_vocab']
+                      ][:20]
+    
+    print('Extracting most negative bigrams...')
+    top_neg_bigrams = [b for b in semantic_sorted_bigrams
+                        if b[0][0] not in opinion_lexicon.vocab['positive_vocab'] 
+                        and b[0][1] not in opinion_lexicon.vocab['positive_vocab'] 
+                        and b[0][0] not in opinion_lexicon.vocab['negative_vocab'] 
+                        and b[0][1] not in opinion_lexicon.vocab['negative_vocab']
+                      ][:-21:-1]
+
+    print('Writing to {0}...'.format(filename))
 
     f.write('Number of tweets analysed: {0}\n\n'.format(number_of_tweets))
-    f.write('Opinion on specifc terms:\n')   
+
+    f.write('Opinion on specifc terms\n')   
     f.write('Barack Obama: ' + str(semantic_orientation_bigrams.get(('barack', 'obama'), 'not present')) + '\n')
     f.write('Donald Trump: ' + str(semantic_orientation_bigrams.get(('donald', 'trump'), 'not present')) + '\n')
     f.write('York Times: ' + str(semantic_orientation_bigrams.get(('york', 'times'), 'not present')) + '\n')
@@ -63,46 +119,47 @@ with open("sentiment_log.txt", "a") as f:
     f.write('Pence: ' + str(semantic_orientation_single.get('pence', 'not present')) + '\n')
     f.write('Michelle: ' + str(semantic_orientation_single.get('michelle', 'not present')) + '\n')
     f.write('Melania: ' + str(semantic_orientation_single.get('melania', 'not present')) + '\n')
+    f.write('\n')
 
-    semantic_sorted_single = sorted(semantic_orientation_single.items(), 
-                       key=operator.itemgetter(1), 
-                       reverse=True)
-    semantic_sorted_bigrams = sorted(semantic_orientation_bigrams.items(), 
-                       key=operator.itemgetter(1), 
-                       reverse=True)
+    f.write('Most positive single terms\n') 
+    f.write('Position | Term | Semantic Orientation\n') 
+    [f.write("{0} | {1} | {2}\n".format(i+1, t, p)) 
+     for i, (t, p) 
+     in enumerate(top_pos_single)]
+    f.write('\n')
 
-    top_pos_single = [b for b in semantic_sorted_single
-                        if b[0] not in opinion_lexicon.vocab['positive_vocab'] 
-                        and b[0] not in opinion_lexicon.vocab['negative_vocab']
-                     ][:20] 
-    top_neg_single = [b for b in semantic_sorted_single
-                        if b[0] not in opinion_lexicon.vocab['positive_vocab'] 
-                        and b[0] not in opinion_lexicon.vocab['negative_vocab']
-                      ][:-21:-1] 
-
-    top_pos_bigrams = [b for b in semantic_sorted_bigrams 
-                        if b[0][0] not in opinion_lexicon.vocab['positive_vocab'] 
-                        and b[0][1] not in opinion_lexicon.vocab['positive_vocab'] 
-                        and b[0][0] not in opinion_lexicon.vocab['negative_vocab'] 
-                        and b[0][1] not in opinion_lexicon.vocab['negative_vocab']
-                      ][:20]
-    top_neg_bigrams = [b for b in semantic_sorted_bigrams
-                        if b[0][0] not in opinion_lexicon.vocab['positive_vocab'] 
-                        and b[0][1] not in opinion_lexicon.vocab['positive_vocab'] 
-                        and b[0][0] not in opinion_lexicon.vocab['negative_vocab'] 
-                        and b[0][1] not in opinion_lexicon.vocab['negative_vocab']
-                      ][:-21:-1]
+    f.write('Most negative single terms\n') 
+    f.write('Position | Term | Semantic Orientation\n') 
+    [f.write("{0} | {1} | {2}\n".format(i+1, t, p)) 
+     for i, (t, p) 
+     in enumerate(top_neg_single)]
+    f.write('\n')
 
     f.write('\n')
-    f.write('Most positive bigrams:\n') 
-    [f.write("{0}: {1} {2}: {3}\n".format(i+1,a,b,p)) for i, ((a,b),p) in enumerate(top_pos_bigrams)]
+    f.write('Most positive bigrams\n')
+    f.write('Position | Terms | Semantic Orientation\n') 
+    [f.write("{0} | {1} {2} | {3}\n".format(i+1, t1, t2, p)) 
+     for i, ((t1, t2), p) 
+     in enumerate(top_pos_bigrams)]
     f.write('\n')
-    f.write('Most negative bigrams:\n') 
-    [f.write("{0}: {1} {2}: {3}\n".format(i+1,a,b,p)) for i, ((a,b),p) in enumerate(top_neg_bigrams)]
+   
+    f.write('Most negative bigrams\n') 
+    f.write('Position | Terms | Semantic Orientation\n') 
+    [f.write("{0} | {1} {2} | {3}\n".format(i+1, t1, t2, p)) 
+     for i, ((t1, t2), p) 
+     in enumerate(top_neg_bigrams)]
     f.write('\n')
-    f.write('Most positive single terms:\n') 
-    [f.write("{0}: {1}: {2}\n".format(i+1,t,p)) for i, (t,p) in enumerate(top_pos_single)]
+   
+    f.write('Most common single terms\n')
+    f.write('Position | Term | Count | Semantic Orientation\n') 
+    [f.write('{0} | {1} | {2} | {3}\n'.format(i+1, t, c, semantic_orientation_single.get(t, 'not present'))) 
+     for i, (t, c) 
+     in enumerate(common_terms)]
     f.write('\n')
-    f.write('Most negative single terms:\n') 
-    [f.write("{0}: {1}: {2}\n".format(i+1,t,p)) for i, (t,p) in enumerate(top_neg_single)]
-    f.write('\n**********\n')
+   
+    f.write('Most common bigrams\n')
+    f.write('Position | Terms | Count | Semantic Orientation\n') 
+    [f.write('{0} | {1} {2} | {3} | {4}\n'.format(i+1,  t1, t2, c, semantic_orientation_bigrams.get((t1, t2), 'not present'))) 
+     for i, ((t1, t2), c) 
+     in enumerate(common_bigrams)]
+    f.write('\n**********\n\n')
